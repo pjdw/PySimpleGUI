c101b74a2599ffc7ad47a084184277bf99e2159c
pjdw <pjdewit@me.com>
2022-12-12 15:22:23 +0100

    Sync local version with github

--- popup_get_date.py
+++ popup_get_date.py
@@ -2,33 +2,35 @@ import datetime as dt
 from datetime import date
 import PySimpleGUI as sg
 
-date_format = '%Y-%m-%d'
-date_choice = dt.date(date.today().year, 1, 1)  # initial date object
-date_choice_str = date_choice.strftime(date_format)  # initial date str
-Ymd = (date_choice.year, date_choice.month, date_choice.day)  # Tuple of Ymd (for popup get date)
+DATE_FORMAT = '%Y-%m-%d'
+DATE_CHOICE = dt.date(date.today().year, 1, 1)  # initial date object
+DATE_CHOICE_STR = DATE_CHOICE.strftime(DATE_FORMAT)  # initial date str
+ymd = (DATE_CHOICE.year, DATE_CHOICE.month, DATE_CHOICE.day)  # Tuple of ymd (for popup get date)
+sg.set_options(font=('Helvetica Neue', 14))
 
 layout = [[sg.T('Chose a date')],
-          [sg.Input(date_choice_str, size=(10, 1), key='-DATE-'), sg.Button('Calender')],
+          [sg.Input(DATE_CHOICE_STR, size=(10, 1), key='-DATE-'), sg.Button('Calender')],
           [sg.Ok(), sg.Exit()]]
 
 window = sg.Window('Date selector', layout)
 
 
-def Cal_popup(a):
-    # initialize date popup with date choice as default. Store choice (tuple) to mdY
-    mdY = sg.popup_get_date(start_year=Ymd[0], start_mon=Ymd[1], start_day=Ymd[2])
-    if mdY:  # If a date is chosen in popup, update input
-        popup_date = dt.date(mdY[2], mdY[0], mdY[1]).strftime(date_format)
-        window['-DATE-'](popup_date)
+def cal_popup():
+    # initialize date popup with date choice as default. Store choice (tuple) to mdy
+    mdy = sg.popup_get_date(start_year=ymd[0], start_mon=ymd[1], start_day=ymd[2])
+    if mdy:  # If a date is chosen in popup, update input
+        popup_date = dt.date(mdy[2], mdy[0], mdy[1]).strftime(DATE_FORMAT)
+        window['-DATE-'](popup_date)  # set input to popup
 
 
-def Check_date(a):
+def check_date(a):
     try:  # check if date is valid; if so => True, else False
-        dt.datetime.strptime(a, date_format)
-        return True
+        dt.datetime.strptime(a, DATE_FORMAT)
     except ValueError:
         sg.popup('Wrong date format', 'Try again')
         return False
+    else:
+        return True
 
 
 while True:
@@ -36,9 +38,9 @@ while True:
     if event in (sg.WIN_CLOSED, 'Exit'):
         break
     elif event == 'Calender':
-        Cal_popup(Ymd)
+        cal_popup()
     elif event == 'Ok':
-        if Check_date(values['-DATE-']):  # If date is OK: update input; else loop again
+        if check_date(values['-DATE-']):  # If date is OK: update input; else loop again
             sg.popup(f"You have chosen {values['-DATE-']} as date", title='Conformation')
             break
         else:
